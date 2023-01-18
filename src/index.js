7f20db1b3893cf1285edbbc5dd11b90ad3a9bac2
yash patel <yashpatel21102002@gmail.com>
2023-01-18 10:29:03 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <>
+    hello its me
+  </>
+  , document.getElementById('root')
+
+)
